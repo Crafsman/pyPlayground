b7e23618bab007a50e185eb12290d39f82a43b42
Yang Mingqian <664811483@qq.com>
2017-01-15 11:46:30 +0800

    First commit source files by pycharm on github

---
+++ rexx.py
@@ -0,0 +1,29 @@
+import re
+import sys
+import urllib.request
+# f_copystr = []
+#
+# file_str = 'where is file?\n i am here'
+# f = open('whereis.txt', 'w')
+# f.writelines(file_str)
+# f.close()
+# old_url = 'http://www.jikexueyuan.com/course/android/?pageNum=2'
+# total_page = 20
+#
+# f = open('text.txt', 'r')
+# html = f.read()
+# f.close()
+
+# for i in range(0, 10):
+#     print(i)
+# for i in sys.path:
+#     print(i)
+#
+# sys.exit('finish')
+
+#Read source.txt file
+f = open('source.txt', 'r')
+html = f.read()
+f.close()
+
+
